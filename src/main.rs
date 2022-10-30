d9401368dc0074ffe97ef13cd301f42ff07fa28a
ppoliani <ppoliani@gmail.com>
2022-10-30 16:18:56 +0200

    feat: setup the main fn

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,27 @@ use serenity::prelude::*;
 use serenity::framework::standard::{StandardFramework};
 use ticketland_pass_bot::{
   utils::store::Store,
-  cmd::{GENERAL_GROUP, Handler},
+  cmd::{GENERAL_GROUP},
+  handler::Handler,
 };
 
 #[tokio::main]
 async fn main() {
+  // Initialize the logger to use environment variables.
+  // In this case, a good default is setting the environment variable
+  // `RUST_LOG` to `debug`.
+  tracing_subscriber::fmt::init();
+
   let store = Store::new().await;
 
   let framework = StandardFramework::new()
   .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
   .group(&GENERAL_GROUP);
 
-  let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+  let intents = GatewayIntents::GUILDS
+  | GatewayIntents::GUILD_MESSAGES
+  | GatewayIntents::MESSAGE_CONTENT;
+
   let mut client = Client::builder(store.config.discord_token, intents)
   .event_handler(Handler)
   .framework(framework)
